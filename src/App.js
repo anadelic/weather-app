62177a83e94bda08a2726530ccf3156cff4c6e1e
Ana Delic <milojevican@gmail.com>
2023-01-21 22:46:35 +0100

    Add some code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { setOriginalNode } from 'typescript';
 
-function App() {
+export default function App() {
+  const [city, setCity] = useState('Vienna');
+  const [temp, setTemp] = useState([]);
+  const [weather, setWeather] = useState([]);
+  const [humidity, setHumidity] = useState([]);
+  const [icon, setIcon] = useState([]);
+  const [name, setName] = useState([]);
+
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
+    )
+    .then(function (res) {
+      // handle success
+      setTemp(res.data.main.temp);
+      setName(res.data.name);
+      setHumidity(res.data.main.humidity);
+
+      setIcon(res.data.weather[0].icon);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+        <h1>{name}</h1>
       </header>
-    </div>
+      <main>
+        <h2> {Math.round(temp - 273)}°C</h2>
+
+        <label forHtml="city">
+          Choose the city
+          <input
+            id="city"
+            name="city"
+            onChange={(e) => {
+              setCity(e.currentTarget.value);
+            }}
+          />
+        </label>
+
+        <p> Humidity: {humidity}%</p>
+
+        <img
+          src={`http://openweathermap.org/img/w/${icon}.png`}
+          alt="weather status icon"
+        />
+      </main>
+      <footer></footer>
+    </body>
   );
 }
-
-export default App;
