9ca9a0d7034cc2da0515b02641ef5bd07d230ebf
Ana Delic <milojevican@gmail.com>
2023-01-23 15:53:48 +0100

    Add app.js

--- src/App.js
+++ src/App.js
@@ -3,61 +3,42 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 export default function App() {
-  const [city, setCity] = useState('Vienna');
-  const [data, setData] = useState({});
-  const [state, setState] = useState('');
+  const [getCity, setGetCity] = useState('London');
+  const [apiData, setApiData] = useState({});
+  const [city, setCity] = useState('London');
 
-  const submitHandler = () => {
-    setState(setCity);
-  };
   const apiKey = process.env.REACT_APP_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-  function ifClicked() {
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
-      )
-      .then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      });
-  }
   useEffect(() => {
-    ifClicked();
-  }, []);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setApiData(data))
+      .catch(() => alert('We could not find that template.'));
+  }, [url]);
 
   return (
     <body>
       <header>
-        <input
-          value={city}
-          onChange={(event) => setCity(event.target.value)}
-          placeholder="Enter Location"
-          type="text"
-        />
-        <button className="location_searcher" onClick={ifClicked}>
-          Search Location
-        </button>
+        <input onChange={(e) => setGetCity(e.target.value)} value={getCity} />
+        <button onClick={(e) => setCity(getCity)}>Search</button>
       </header>
-      <section>
-        <h1> {data.name}</h1>
-        <h2>{Math.round(data.main.temp - 273)} °C</h2>
-        <p>Humidity: {data.main.humidity} %</p>
-      </section>
-      <section>
-        <p>High: {Math.round(data.main.temp_max - 273)} °C</p>
-        <p>Low: {Math.round(data.main.temp_min - 273)} °C</p>
-      </section>
-      <section>
+      <main>
+        <h1> {apiData.name}</h1>
+        <h2>{Math.round(apiData.main.temp - 273)} °C</h2>
+        <p>Humidity: {apiData.main.humidity} %</p>
+
+        <section>
+          <p>High: {Math.round(apiData.main.temp_max - 273)} °C</p>
+          <p>Low: {Math.round(apiData.main.temp_min - 273)} °C</p>
+        </section>
+
         <img
-          src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+          src={`http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`}
           alt="weather status icon"
         />
-      </section>
+      </main>
+
       <footer> Weather app made by Ana</footer>
     </body>
   );
