0955dc37e549033a2847ba4fa008d19d3174f918
Ana Delic <milojevican@gmail.com>
2023-01-24 14:28:07 +0100

    Add optional chaining operator

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 
 export default function App() {
-  const [getCity, setGetCity] = useState('London');
-  const [apiData, setApiData] = useState({});
+  const [apiData, setApiData] = useState([]);
   const [city, setCity] = useState('London');
+  const [getCity, setGetCity] = useState('');
 
   const apiKey = process.env.REACT_APP_API_KEY;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
@@ -19,26 +19,27 @@ export default function App() {
   return (
     <body>
       <header>
-        <input onChange={(e) => setGetCity(e.target.value)} value={getCity} />
+        <input
+          onChange={(e) => setGetCity(e.target.value)}
+          value={getCity}
+          placeholder="Enter a city name"
+        />
         <button onClick={() => setCity(getCity)}>Search</button>
       </header>
       <main>
         <h1> {apiData.name}</h1>
-        <h2>{Math.round(apiData.main.temp - 273)} °C</h2>
-        <p>Humidity: {apiData.main.humidity} %</p>
+        <h2>{Math.round(apiData.main?.temp - 273)} °C</h2>
+        <p>Humidity: {apiData.main?.humidity} %</p>
 
         <section>
-          <p>High: {Math.round(apiData.main.temp_max - 273)} °C</p>
-          <p>Low: {Math.round(apiData.main.temp_min - 273)} °C</p>
+          <div className="tempMaxMin">
+            <p>High: {Math.round(apiData.main?.temp_max - 273)} °C</p>{' '}
+          </div>
+          <div className="tempMaxMin">
+            <p>Low: {Math.round(apiData.main?.temp_min - 273)} °C</p>{' '}
+          </div>
         </section>
-
-        <img
-          src={`http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`}
-          alt="weather status icon"
-        />
       </main>
-
-      <footer> Weather app made by Ana</footer>
     </body>
   );
 }
